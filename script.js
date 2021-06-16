050a4f0e4902aa94eb8451b0f86d6c18c4674bf8
matchagn <masaki.matsumoto.358@gmail.com>
2021-06-17 08:55:13 +0900

    テスト

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
-function sound1() {
-  // 対象となるID名
-  var id = "sound-file1";
+function sound()
+{
+	// 対象となるID名
+	var id = 'sound-file' ;
 
-  // 初回以外だったら音声ファイルを巻き戻す
-  if (typeof document.getElementById(id).currentTime != "undefined") {
-    document.getElementById(id).currentTime = 0;
-  }
+	// 初回以外だったら音声ファイルを巻き戻す
+	if( typeof( document.getElementById( id ).currentTime ) != 'undefined' )
+	{
+		document.getElementById( id ).currentTime = 0;
+	}
 
-  // [ID:sound-file]の音声ファイルを再生[play()]する
-  document.getElementById(id).play();
-}
-
-function sound2() {
-  // 対象となるID名
-  var id = "sound-file2";
-
-  // 初回以外だったら音声ファイルを巻き戻す
-  if (typeof document.getElementById(id).currentTime != "undefined") {
-    document.getElementById(id).currentTime = 0;
-  }
-
-  // [ID:sound-file]の音声ファイルを再生[play()]する
-  document.getElementById(id).play();
-}
-
-function sound3() {
-  // 対象となるID名
-  var id = "sound-file3";
-
-  // 初回以外だったら音声ファイルを巻き戻す
-  if (typeof document.getElementById(id).currentTime != "undefined") {
-    document.getElementById(id).currentTime = 0;
-  }
-
-  // [ID:sound-file]の音声ファイルを再生[play()]する
-  document.getElementById(id).play();
+	// [ID:sound-file]の音声ファイルを再生[play()]する
+	document.getElementById( id ).play() ;
 }
